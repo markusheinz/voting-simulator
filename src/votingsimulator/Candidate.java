fe009288b5a39ba76750672b4ab5919492a73e56
Markus Heinz <markus@computer5>
2021-09-17 20:01:33 +0200

    Refactoring of election processing
    Vote counts are now stored with AtomicIntegers in Candidate objects. 
    Rankings and votes are now stored as class constants instead of local 
    variables in a method.

--- src/votingsimulator/Candidate.java
+++ src/votingsimulator/Candidate.java
@@ -1,23 +1,33 @@
 package votingsimulator;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Candidate implements Comparable<Candidate> {
     protected int candidateId;
-    protected int voteCount;
+    protected AtomicInteger voteCount;
     
-    public Candidate(int id, int count) {
+    public Candidate(int id) {
         candidateId = id;
-        voteCount = count;
+        voteCount = new AtomicInteger();
+    }
+
+    public void addVote() {
+        addVote(1);
+    }
+
+    public void addVote(int points) {
+        voteCount.getAndAdd(points);
     }
 
     @Override
     public int compareTo(Candidate o) {
-        return o.voteCount - voteCount;
+        return o.voteCount.get() - voteCount.get();
     }
     
     @Override
     public String toString() {
         return String.format("Candidate %02d has %10d votes.", 
-                candidateId, voteCount);
+                candidateId, voteCount.get());
     }
             
 }
--- src/votingsimulator/VotingSimulator.java
+++ src/votingsimulator/VotingSimulator.java
@@ -7,20 +7,23 @@ public class VotingSimulator {
     public static final int MAX_CANDIDATE = 30;
     public static final int[] POINTS = {5, 4, 3};    
     public static int VOTERS = 60000000;
+
+    protected static final Vote[] votes = new Vote[VOTERS];
+
+    protected static final Candidate[] rankingMulti =
+            new Candidate[MAX_CANDIDATE];
+
+    protected static final Candidate[] rankingSingle =
+            new Candidate[MAX_CANDIDATE];
     
     public static void main(String[] args) {
         doElection();
     }
     
     protected static void doElection() {
-        Vote[] votes = new Vote[VOTERS];
-        
-        int[] rankingMulti = new int[MAX_CANDIDATE];
-        int[] rankingSingle = new int [MAX_CANDIDATE];
-        
         for (int i = 0; i < MAX_CANDIDATE; i++) {
-            rankingMulti[i] = 0;
-            rankingSingle[i] = 0;
+            rankingMulti[i] = new Candidate(i);
+            rankingSingle[i] = new Candidate(i);
         }
         
         for (Vote vote : votes) {
@@ -30,29 +33,22 @@ public class VotingSimulator {
             for (Entry<Integer, Integer> iterator : 
                     vote.getMultiVote().entrySet()) {
                 
-                rankingMulti[iterator.getKey()] += iterator.getValue();
+                rankingMulti[iterator.getKey()].addVote(iterator.getValue());
             }
             
-            
             int singleVote = vote.getSingleVote();
-            rankingSingle[singleVote]++;
+            rankingSingle[singleVote].addVote();
         }
         
         printRanking(rankingSingle);
         printRanking(rankingMulti);
     }
     
-    protected static void printRanking(int [] ranking) {
-        Candidate[] candidates = new Candidate[MAX_CANDIDATE];
+    protected static void printRanking(Candidate[] ranking) {
+        Arrays.sort(ranking);
         
         for (int i = 0; i < MAX_CANDIDATE; i++) {
-            candidates[i] = new Candidate(i, ranking[i]);
-        }
-        
-        Arrays.sort(candidates);
-        
-        for (int i = 0; i < MAX_CANDIDATE; i++) {
-            System.out.println(candidates[i].toString());
+            System.out.println(ranking[i].toString());
         }
         
         System.out.println();
